94977be858a0117060b36c576b1db15f0eda0098
Felipe Savoia <fsavoia@amazon.com>
2023-01-27 10:11:17 -0300

    changes

--- workshops/ecs/README.md
+++
@@ -1,6 +0,0 @@
-# LATAM Containers Roadshow - Amazon ECS Workshop
-
-Please select your preferred language:
-
-- [Brazilian Portuguese (Português do Brasil)](./docs/pt_BR/README.md)
-- [Spanish (Español) - TBA](#)
--- workshops/ecs/src/frontend/webapp/src/components/util.ts
+++
@@ -1,8 +0,0 @@
-export function stringToBool(b: string) {
-    // convert string "true" => true and "false" => false
-    switch (b) {
-        case "true": return true
-        case "false": return false
-        default: return false
-    }
-}
